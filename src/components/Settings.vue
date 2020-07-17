9f84dd0d14ed524f26864c8e0fe9cf991c0dcb97
Joona Somerkivi <joona.somerkivi@nordcloud.com>
2020-07-17 08:52:57 +0300

    feat: Placeholders for diffirent type of settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,62 +1,134 @@
 <template>
-  <div class="referee-form">
-    <div class="field">
-      <b-field>
-        <b-input 
-          v-model="referee.firstName" 
-          required
-          placeholder="Given name" 
-          :expanded="true"
-        ></b-input>
-        <b-input 
-          v-model="referee.lastName" 
-          required
-          placeholder="Family name" 
-          :expanded="true"
-        ></b-input>
-      </b-field>
-    </div>
-    <div class="field">
-      <b-field label="Nationality" label-position="on-border">
-        <b-autocomplete
-            v-model="countryQuery"
-            placeholder="As in passport"
-            icon="passport"
-            :keep-first="true"
-            required
-            :data="getCountries(countryQuery)"
-            :expanded="true"
-            @select="option => referee.country = option"
-        >
-        </b-autocomplete>
-      </b-field>
-    </div>
-    <div class="field">
-      <b-field label="Level" label-position="on-border">
-        <b-radio-button v-model="referee.level" :native-value="1" >
-            1
-        </b-radio-button>
-        <b-radio-button v-model="referee.level" :native-value="2" >
-            2
-        </b-radio-button>
-        <b-radio-button v-model="referee.level" :native-value="3" >
-            3
-        </b-radio-button>
-        <b-radio-button v-model="referee.level" :native-value="0" >
-            National
-        </b-radio-button>
-      </b-field>
-    </div>
-    <div class="field">
-      <b-button 
-        type="is-primary" 
-        @click="handleSave" 
-        icon-left="account-check"
-        v-bind:loading="isLoading"
-      >
-        Save</b-button>
-      <button class="button is-text" @click="handleCancel">Cancel</button>
-    </div>
+  <div>
+    <!-- PROFILE -->
+    <b-collapse
+      class="card"
+      animation="slide"
+      :open="isOpen == P"
+      @open="isOpen = P"
+    >
+      <div
+          slot="trigger"
+          slot-scope="props"
+          class="card-header"
+          role="button">
+          <span class="card-header-title">Profile</span>
+          <a class="card-header-icon">
+              <b-icon
+                  :icon="props.open ? 'menu-down' : 'menu-up'">
+              </b-icon>
+          </a>
+      </div>
+      <div class="card-content">
+        <div class="content referee-form">
+          <div class="field">
+            <b-field>
+              <b-input 
+                v-model="referee.firstName" 
+                required
+                placeholder="Given name" 
+                :expanded="true"
+              ></b-input>
+              <b-input 
+                v-model="referee.lastName" 
+                required
+                placeholder="Family name" 
+                :expanded="true"
+              ></b-input>
+            </b-field>
+          </div>
+          <div class="field">
+            <b-field label="Nationality" label-position="on-border">
+              <b-autocomplete
+                  v-model="countryQuery"
+                  placeholder="As in passport"
+                  icon="passport"
+                  :keep-first="true"
+                  required
+                  :data="getCountries(countryQuery)"
+                  :expanded="true"
+                  @select="option => referee.country = option"
+              >
+              </b-autocomplete>
+            </b-field>
+          </div>
+          <div class="field">
+            <b-field label="Level" label-position="on-border">
+              <b-radio-button v-model="referee.level" :native-value="1" >
+                  1
+              </b-radio-button>
+              <b-radio-button v-model="referee.level" :native-value="2" >
+                  2
+              </b-radio-button>
+              <b-radio-button v-model="referee.level" :native-value="3" >
+                  3
+              </b-radio-button>
+              <b-radio-button v-model="referee.level" :native-value="0" >
+                  National
+              </b-radio-button>
+            </b-field>
+          </div>
+          <div class="field">
+            <b-button 
+              type="is-primary" 
+              @click="handleSave" 
+              icon-left="account-check"
+              v-bind:loading="isLoading"
+            >
+              Save</b-button>
+            <button class="button is-text" @click="handleCancel">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </b-collapse><!-- /PROFILE -->
+
+    <!-- SECURITY -->
+    <b-collapse
+      class="card"
+      animation="slide"
+      :open="isOpen == S"
+      @open="isOpen = S"
+    >
+      <div
+          slot="trigger"
+          slot-scope="props"
+          class="card-header"
+          role="button">
+          <span class="card-header-title">Security</span>
+          <a class="card-header-icon">
+              <b-icon
+                  :icon="props.open ? 'menu-down' : 'menu-up'">
+              </b-icon>
+          </a>
+      </div>
+      <div class="card-content">
+        Placeholder for email/password/2fa settings
+      </div>
+    </b-collapse><!-- /SERCURITY -->
+    <!-- ASSOCIATION -->
+    <b-collapse
+      class="card"
+      animation="slide"
+      :open="isOpen == N"
+      @open="isOpen = N"
+    >
+      <div
+          slot="trigger"
+          slot-scope="props"
+          class="card-header"
+          role="button">
+          <span class="card-header-title">National Association</span>
+          <a class="card-header-icon">
+              <b-icon
+                  :icon="props.open ? 'menu-down' : 'menu-up'">
+              </b-icon>
+          </a>
+      </div>
+      <div class="card-content">
+        Placeholder for national association of {{ referee.country }} settings.
+        Or whatever country the referee lives in...
+      </div>
+    </b-collapse><!-- /ASSOCIATION -->
   </div>
 </template>
 <style lang="css">
@@ -66,11 +138,14 @@
 </style>
 <script>
 import Referee from "../store/models/RefereeClass";
-
 export default {
   data: () => {
     return {
-      referee: new Referee({ id: null }),
+      S: 'security',
+      P: 'profile',
+      N: 'national-association',
+      isOpen: 'profile',
+      referee: new Referee(),
       password: "",
       countryQuery: "",
     }
